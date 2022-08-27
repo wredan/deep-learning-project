ec62b5bad22592371d468f787affdb6beba2ced4
Danilo Santitto <danilo.santitto@gmail.com>
2022-08-27 18:03:04 +0200

    feat(extract_patches): add class ExtractPatches
    make scripts a module

--- scripts/extract_patches.py
+++ scripts/extract_patches.py
@@ -1,44 +1,46 @@
 
 import fiftyone as fo
-import sys
 
-if len(sys.argv) < 3:
-    print("\nThis script gets al least 2 parameters: first one is base path of the datasets, second one is base path where to save the patches.\n")
-    sys.exit(0)
+class ExtractPatches():
 
-for i in range(0, 5):
-    if i == 0:
-        image_sub_path = "real/test"
-    elif i == 1:
-        image_sub_path = "real/training"
-    elif i == 2:
-        image_sub_path = "syntehtic/test"
-    elif i == 3:
-        image_sub_path = "syntehtic/training"
-    elif i == 4:
-        image_sub_path = "syntehtic/validation"
+    def __init__(self, main_path, save_path):
+        self.main_path = main_path
+        self.save_path = save_path
 
-    # The path to the source images
-    image_path = sys.argv[1] + image_sub_path
+    def extract(self):
+        for i in range(0, 5):
+            if i == 0:
+                image_sub_path = "real/test"
+            elif i == 1:
+                image_sub_path = "real/training"
+            elif i == 2:
+                image_sub_path = "syntehtic/test"
+            elif i == 3:
+                image_sub_path = "syntehtic/training"
+            elif i == 4:
+                image_sub_path = "syntehtic/validation"
 
-    # The path to the COCO labels JSON file
-    labels_path = sys.argv[1] + image_sub_path + "/annotations.json"
+            # The path to the source images
+            image_path = self.main_path + image_sub_path
 
-    # Import the dataset
-    dataset = fo.Dataset.from_dir(
-        dataset_type=fo.types.COCODetectionDataset,
-        data_path=image_path,
-        labels_path=labels_path,
-        name=image_sub_path
-    )
+            # The path to the COCO labels JSON file
+            labels_path = self.main_path + image_sub_path + "/annotations.json"
 
-    patches = dataset.to_patches("ground_truth")
+            # Import the dataset
+            dataset = fo.Dataset.from_dir(
+                dataset_type=fo.types.COCODetectionDataset,
+                data_path=image_path,
+                labels_path=labels_path,
+                name=image_sub_path
+            )
 
-    # https://voxel51.com/docs/fiftyone/api/fiftyone.types.dataset_types.html
-    # dataset_type=fo.types.FiftyOneImageClassificationDataset, patch dentro la cartella "data" + labels.json
-    # dataset_type=fo.types.ImageClassificationDirectoryTree, divide per cartelle le patch in base alla classe
-    patches.export(
-        export_dir=sys.argv[2] + image_sub_path,
-        dataset_type=fo.types.FiftyOneImageClassificationDataset,
-        label_field="ground_truth",
-    )
+            patches = dataset.to_patches("ground_truth")
+
+            # https://voxel51.com/docs/fiftyone/api/fiftyone.types.dataset_types.html
+            # dataset_type=fo.types.FiftyOneImageClassificationDataset, patch dentro la cartella "data" + labels.json
+            # dataset_type=fo.types.ImageClassificationDirectoryTree, divide per cartelle le patch in base alla classe
+            patches.export(
+                export_dir=self.save_path + image_sub_path,
+                dataset_type=fo.types.FiftyOneImageClassificationDataset,
+                label_field="ground_truth",
+            )
