78c3a24e63057bb159b3b452c70c5d7f01101caa
Danilo Santitto <danilo.santitto@gmail.com>
2022-11-09 17:35:41 +0100

    feat(GRL): adapt data_module and dataset

--- scripts/dataset/GRL/data_module_GRL.py
+++ scripts/dataset/GRL/data_module_GRL.py
@@ -1,14 +1,14 @@
-from scripts.dataset.GAN.dataset_GAN import CulturalSiteDatasetGAN
+from scripts.dataset.GRL.dataset_GRL import CulturalSiteDatasetGRL
 import os
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from PIL import Image
+from sklearn.model_selection import train_test_split
 
 # Global Variable
 BASE_CLASS_DATASETS_PATH = os.path.join(os.getcwd(), 'CLASS-EGO-CH-OBJ-ADAPT')
 
-class CulturalSiteDataModuleGAN(pl.LightningDataModule):
+class CulturalSiteDataModuleGRL(pl.LightningDataModule):
 
     FIT_STAGE = 0
     TEST_STAGE = 1
@@ -22,44 +22,58 @@ class CulturalSiteDataModuleGAN(pl.LightningDataModule):
         self.cultural_site_test = None            
 
     def setup(self, stage=ALL_STAGE):
-        if stage==CulturalSiteDataModuleGAN.FIT_STAGE or stage==CulturalSiteDataModuleGAN.ALL_STAGE:
+        test_val_transform = transforms.Compose([
+            transforms.Resize(256, transforms.InterpolationMode.BICUBIC),
+            transforms.CenterCrop(224),                                                         
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))  # TODO: rivedere sti valori
+        ])
+
+        if stage==CulturalSiteDataModuleGRL.FIT_STAGE or stage==CulturalSiteDataModuleGRL.ALL_STAGE:
             train_transform = transforms.Compose([
                 transforms.Resize(256, transforms.InterpolationMode.BICUBIC), #ridimensioniamo a una dimensione più grande di quella di input
                 transforms.RandomCrop(224), #random crop alla dimensione di input
                 transforms.RandomHorizontalFlip(), #random flip orizzontale
                 transforms.ToTensor(), #trasformiamo in tensore
-                #? applichiamo la normalizzazione (detto dal prof furnari: solitamente quando si usano le GAN non si fanno grosse assunzioni, 
-                #? va bene lasciare una media e std intorno ai volori 0.5 sotto proposti)
-                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) # TODO: rivedere sti valori
             ])
 
-            self.cultural_site_train = CulturalSiteDatasetGAN(
+            self.cultural_site_train = CulturalSiteDatasetGRL(
                 dataset_base_path=BASE_CLASS_DATASETS_PATH,
-                transform= train_transform,
-                mode=CulturalSiteDatasetGAN.TRAIN)
+                dataset_stage=CulturalSiteDatasetGRL.TRAIN, 
+                transform=train_transform)
 
-        if stage==CulturalSiteDataModuleGAN.TEST_STAGE or stage==CulturalSiteDataModuleGAN.ALL_STAGE:
-            test_transform = transforms.Compose([
-                transforms.Resize(256, transforms.InterpolationMode.BICUBIC),
-                transforms.CenterCrop(224),                                                         
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
-            ])
-
-            self.cultural_site_test = CulturalSiteDatasetGAN(
+            train_set = self.cultural_site_train.get_real_image_dataset()
+            new_train_set, new_validation_set = train_test_split(train_set, test_size = 0.1) # splitting train ratio 90/10
+            self.cultural_site_train.set_real_image_dataset(new_train_set)
+            
+            self.cultural_site_val = CulturalSiteDatasetGRL(
                 dataset_base_path=BASE_CLASS_DATASETS_PATH,
-                transform= test_transform,
-                mode=CulturalSiteDatasetGAN.TEST)
+                dataset_stage=CulturalSiteDatasetGRL.VALIDATION, 
+                transform=test_val_transform)
+            self.cultural_site_val.set_real_image_dataset(new_validation_set)
+
+        if stage==CulturalSiteDataModuleGRL.TEST_STAGE or stage==CulturalSiteDataModuleGRL.ALL_STAGE:
+            self.cultural_site_test = CulturalSiteDatasetGRL(
+                dataset_base_path=BASE_CLASS_DATASETS_PATH,
+                dataset_stage=CulturalSiteDatasetGRL.TEST, 
+                transform=test_val_transform)
 
     def filter_train(self, pixel_threshold):
         self.cultural_site_train.filter_dataset(pixel_threshold)
+
+    def filter_val(self, pixel_threshold):
+        self.cultural_site_val.filter_dataset(pixel_threshold)
 
     def filter_test(self, pixel_threshold):
         self.cultural_site_test.filter_dataset(pixel_threshold)
     
     def train_dataloader(self):
         return DataLoader(self.cultural_site_train, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+        
+    def val_dataloader(self):
+        return DataLoader(self.cultural_site_val, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def test_dataloader(self):
         return DataLoader(self.cultural_site_test, batch_size=self.batch_size, num_workers=self.num_workers)
--- scripts/dataset/GRL/dataset_GRL.py
+++ scripts/dataset/GRL/dataset_GRL.py
@@ -4,43 +4,96 @@ import os
 import glob
 from typing import Callable, Optional
 from torch.utils.data import Dataset
+import json
+import numpy as np
 
-class CulturalSiteDatasetGAN(Dataset):
+class CulturalSiteDatasetGRL(Dataset):
 
     TRAIN = "training"
-    TEST = "test"
+    VALIDATION = "test"
+    TEST = "validation"
 
     REAL = "real"
     SYNTHETIC = "syntehtic"
 
-    def __init__(self, dataset_base_path, transform: Optional[Callable] = None, mode= TRAIN) -> None:
-        self.transform = transform
+    def __init__(self, dataset_base_path=None, dataset_stage=TRAIN, transform: Optional[Callable] = None) -> None:
+            if dataset_base_path is not None:
+                self.transform = transform
+                if dataset_stage == CulturalSiteDatasetGRL.TRAIN or dataset_stage == CulturalSiteDatasetGRL.TEST:
+                    dataset_folder_syn, labels_file_syn, dataset_folder_real, labels_file_real = self.__get_train_or_test_path(dataset_base_path, dataset_stage)
+                    self.load_datasets(dataset_folder_syn, labels_file_syn, dataset_folder_real, labels_file_real)
+                elif dataset_stage == CulturalSiteDatasetGRL.VALIDATION:
+                    dataset_folder_syn = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.SYNTHETIC, 'validation', 'data')
+                    labels_file_syn = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.SYNTHETIC, 'validation', 'labels.json')
+                    self.load_datasets(dataset_folder_syn, labels_file_syn)
+            else:
+                self.syn_image_dataset = []
+                self.real_image_dataset = []
 
-        # ottieni i path delle immagini in A e B
-        self.images_path_syn = sorted(glob.glob(os.path.join(dataset_base_path, CulturalSiteDatasetGAN.SYNTHETIC, '%s' % mode, 'data') + '/*.*'))
-        self.images_path_real = sorted(glob.glob(os.path.join(dataset_base_path, CulturalSiteDatasetGAN.REAL, '%s' % mode, 'data') + '/*.*'))
+    def __get_train_or_test_path(self, dataset_base_path, mode):
+        dataset_folder_syn = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.SYNTHETIC, '%s' % mode, 'data')
+        labels_file_syn = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.SYNTHETIC, '%s' % mode, 'labels.json')
+        dataset_folder_real = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.REAL, '%s' % mode, 'data')
+        labels_file_real = os.path.join(dataset_base_path, CulturalSiteDatasetGRL.REAL, '%s' % mode, 'labels.json')
+        return dataset_folder_syn, labels_file_syn, dataset_folder_real, labels_file_real
+
+    def get_syn_image_dataset(self):
+        return self.syn_image_dataset
+
+    def set_syn_image_dataset(self, image_dataset):
+        self.syn_image_dataset = np.array(image_dataset)
+
+    def get_real_image_dataset(self):
+        return self.real_image_dataset
+
+    def set_real_image_dataset(self, image_dataset):
+        self.real_image_dataset = np.array(image_dataset)
+
+    def load_datasets(self, dataset_folder_syn, labels_file_syn, dataset_folder_real=None, labels_file_real=None):
+        self.syn_image_dataset = self._load_images_path(dataset_folder_syn)
+        self.syn_image_dataset = self._load_image_classes(self.syn_image_dataset, labels_file_syn, os.path.join(os.getcwd(), "utils", "image_classes.json"))
+        self.syn_image_dataset = np.array(self.syn_image_dataset, dtype=object)
+
+        if dataset_folder_real:
+            self.real_image_dataset = self._load_images_path(dataset_folder_real)
+            self.real_image_dataset = self._load_image_classes(self.real_image_dataset, labels_file_real, os.path.join(os.getcwd(), "utils", "image_classes.json"))
+            self.real_image_dataset = np.array(self.real_image_dataset, dtype=object)
+    
+    def _load_images_path(self, path):
+        image_dataset = []
+        suffix = ".jpg"
+        for filename in os.listdir(path):
+            image_dataset.append([filename.removesuffix(suffix), os.path.join(path, filename), None ])
+        return image_dataset
+
+    def _load_image_classes(self, image_dataset, labels_path, class_ids_path):
+        labels_content = json.load(open(labels_path))
+        ids_content = json.load(open(class_ids_path))
+        for img in image_dataset:
+            for class_el in ids_content["categories"]:
+                if class_el["name"] == labels_content["labels"][img[0]]:
+                     img[2] = class_el["id"]
+        return image_dataset
 
     def __check_filter_size(self, img_path, pixel_threshold):
         w, h = Image.open(img_path).size
         return w > pixel_threshold and h > pixel_threshold
 
     def filter_dataset(self, pixel_threshold):
-        tmp = [x for x in self.images_path_syn if self.__check_filter_size(x, pixel_threshold)]
-        self.images_path_syn = tmp
-        tmp = [x for x in self.images_path_real if self.__check_filter_size(x, pixel_threshold)]
-        self.images_path_real = tmp
+        tmp = [x for x in self.syn_image_dataset if self.__check_filter_size(x, pixel_threshold)]
+        self.syn_image_dataset = tmp
+        tmp = [x for x in self.real_image_dataset if self.__check_filter_size(x, pixel_threshold)]
+        self.real_image_dataset = tmp
 
     def __getitem__(self, index: int):
-        #apro l'iesima immagine A (uso il modulo per evitare di sforare)
-        item_A = Image.open(self.images_path_syn[index % len(self.images_path_syn)])
-        #apro una immagine B a caso
-        item_B = Image.open(self.images_path_real[random.randint(0, len(self.images_path_real) - 1)])
-        
+        _, syn_img_path, syn_img_class = self.syn_image_dataset[index]
+        _, real_img_path, _ = self.real_image_dataset[random.randint(0, len(self.target)-1)]
+        syn_img = Image.open(syn_img_path)
+        real_img = Image.open(real_img_path)
         if self.transform is not None:
-            item_A = self.transform(item_A)
-            item_B = self.transform(item_B)
-
-        return item_A, item_B
+            transf_img_syn = self.transform(syn_img)
+            transf_img_real = self.transform(real_img)
+        return transf_img_syn, transf_img_real, syn_img_class
 
     def __len__(self) -> int:
-        return max(len(self.images_path_syn), len(self.images_path_real))
+        return len(self.syn_image_dataset)
