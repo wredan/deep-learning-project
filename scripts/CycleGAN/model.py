fccc17f6e0d707aa98cf2eaec54d54e7e8bd187f
Danilo Santitto <danilo.santitto@gmail.com>
2022-11-15 22:31:22 +0100

    reafactor(CycleGAN): addCycleGAN model file

--- scripts/CycleGAN/model.py
+++ scripts/CycleGAN/model.py
@@ -1,45 +1,43 @@
 import pytorch_lightning as pl
 import itertools
-import torch
 import torchvision
 from scripts.CycleGAN.generator import Generator
 from scripts.CycleGAN.discriminator import Discriminator
 from scripts.CycleGAN.utils import weights_init_normal, LambdaLR
-
+import torch
 class CycleGAN(pl.LightningModule):
 
     A2B = 0
     B2A = 1
 
     def __init__(self, 
-                 input_nc = 3, # numero di canali in input
-                 output_nc = 3, # numero di canali in output
-                 lr=0.0002, # learning rate
-                 betas=(0.5, 0.999), # valori beta di Adam
-                 starting_epoch=0, # epoca di partenza (0 se non stiamo facendo resume di un training interrotto)
-                 n_epochs=200, # numero totale di epoche
-                 decay_epoch=100, # epoca dopo la quale iniziare a far scendere il learning rate
-                 batch_size = 1, #batch size
+                 input_nc = 3, 
+                 output_nc = 3, 
+                 lr=0.0002, 
+                 betas=(0.5, 0.999), 
+                 starting_epoch=0, 
+                 n_epochs=200, 
+                 decay_epoch=100,
                  print_images_each_N_batch = 500
                   ):
         super(CycleGAN, self).__init__()
         self.save_hyperparameters()
         
-        # Definiamo due generatori: da A a B e da B ad A
+        # Generators A2B, B2A
         self.netG_A2B = Generator(input_nc, output_nc)
         self.netG_B2A = Generator(output_nc, input_nc)
         
-        # Definiamo due discriminatori: uno per A e uno per B
+        # A and B Discriminator
         self.netD_A = Discriminator(input_nc)
         self.netD_B = Discriminator(output_nc)
         
-        # Applichiamo la normalizzazione
+        # Normalization
         self.netG_A2B.apply(weights_init_normal)
         self.netG_B2A.apply(weights_init_normal)
         self.netD_A.apply(weights_init_normal)
         self.netD_B.apply(weights_init_normal)
         
-        # Definiamo le loss
+        # Losses
         self.criterion_GAN = torch.nn.MSELoss()
         self.criterion_cycle = torch.nn.L1Loss()
         self.criterion_identity = torch.nn.L1Loss()
@@ -50,22 +48,20 @@ class CycleGAN(pl.LightningModule):
         else:
             return self.netG_B2A(x)
     
-    # ci servono 3 optimizer, ognuno con il suo scheduler
     def configure_optimizers(self):
-        # Optimizer per il generatore
+        # Generator Optimizer
         optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A2B.parameters(), self.netG_B2A.parameters()),
                                 lr=self.hparams.lr, betas=self.hparams.betas)
         
-        # Optimizers per i due discriminatori
+        # Discriminator Optimizers
         optimizer_D_A = torch.optim.Adam(self.netD_A.parameters(), lr=self.hparams.lr, betas=self.hparams.betas)
         optimizer_D_B = torch.optim.Adam(self.netD_B.parameters(), lr=self.hparams.lr, betas=self.hparams.betas)
         
-        # Questi scheduler fanno decadere il learning rate dopo 100 epoche
+        # Scheduler learning rate decay
         lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(self.hparams.n_epochs, self.hparams.starting_epoch, self.hparams.decay_epoch).step)
         lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(self.hparams.n_epochs, self.hparams.starting_epoch, self.hparams.decay_epoch).step)
         lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(self.hparams.n_epochs, self.hparams.starting_epoch, self.hparams.decay_epoch).step)
         
-        # Restituiamo i tre optimizer e i tre optimizers
         return [optimizer_G, optimizer_D_A, optimizer_D_B], [lr_scheduler_G, lr_scheduler_D_A, lr_scheduler_D_B]
     
     
@@ -75,34 +71,34 @@ class CycleGAN(pl.LightningModule):
         target_real = torch.ones((real_A.shape[0],1)).type_as(real_A)
         target_fake = torch.zeros((real_A.shape[0],1)).type_as(real_A)
         
-        # optimizer del generatore
+        # Generator
         if optimizer_idx==0:
             # Identity loss
             # G_A2B(B) should equal B if real B is fed
-            same_B = self.netG_A2B(real_B) # passa B al generatore A2B - deve essere uguale a B
+            same_B = self.netG_A2B(real_B)
             loss_identity_B = self.criterion_identity(same_B, real_B)*5.0
 
             # G_B2A(A) should equal A if real A is fed
-            same_A = self.netG_B2A(real_A) # passa A al generatore B2A - deve essere uguale ad A
+            same_A = self.netG_B2A(real_A)
             loss_identity_A = self.criterion_identity(same_A, real_A)*5.0
 
             # GAN loss
-            fake_B = self.netG_A2B(real_A) # passa A ad A2B - sono le fake B
-            pred_fake = self.netD_B(fake_B) # predizioni del discriminatore B
-            loss_GAN_A2B = self.criterion_GAN(pred_fake, target_real) # loss GAN per A2B
+            fake_B = self.netG_A2B(real_A) 
+            pred_fake = self.netD_B(fake_B) # discriminator B prediction
+            loss_GAN_A2B = self.criterion_GAN(pred_fake, target_real) # loss GAN A2B
 
-            fake_A = self.netG_B2A(real_B) # passa B a B2A - sono le fake A
-            pred_fake = self.netD_A(fake_A) # predizioni del discriminatore A
-            loss_GAN_B2A = self.criterion_GAN(pred_fake, target_real) # loss GAN per B2A
+            fake_A = self.netG_B2A(real_B)
+            pred_fake = self.netD_A(fake_A)  # discriminator A prediction
+            loss_GAN_B2A = self.criterion_GAN(pred_fake, target_real) # loss GAN B2A
 
             # Cycle consistency loss
-            recovered_A = self.netG_B2A(fake_B) #passiamo le fake B a B2A - devono essere uguali a real_A
+            recovered_A = self.netG_B2A(fake_B) #fake B -> B2A - should be same as real_A
             loss_cycle_ABA = self.criterion_cycle(recovered_A, real_A)*10.0 #cycle consistency loss
 
-            recovered_B = self.netG_A2B(fake_A) #passiamo le fake A a A2B - devono essere uguali a real_B
+            recovered_B = self.netG_A2B(fake_A) #fake A -> A2B - should be same as real_B
             loss_cycle_BAB = self.criterion_cycle(recovered_B, real_B)*10.0 #cycle consistency loss
 
-            # loss globale
+            # Overall loss
             loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
             self.log('loss_G/loss_identity_A', loss_identity_A, on_epoch=True)
             self.log('loss_G/loss_identity_B', loss_identity_B, on_epoch=True)
@@ -112,7 +108,7 @@ class CycleGAN(pl.LightningModule):
             self.log('loss_G/loss_cycle_BAB', loss_cycle_BAB, on_epoch=True)
             self.log('loss_G/overall', loss_G, on_epoch=True)
             
-            # loggiamo dei campioni visivi da ispezionare durante il training ogni 100 batch
+            # Log images to visually check the trainig
             if batch_idx % self.hparams.print_images_each_N_batch == 0:
                 grid_A = torchvision.utils.make_grid(real_A[:50], nrow=10, normalize=True)
                 grid_A2B = torchvision.utils.make_grid(fake_B[:50], nrow=10, normalize=True)
@@ -132,13 +128,14 @@ class CycleGAN(pl.LightningModule):
                 
             return loss_G
         
-        elif optimizer_idx==1: #discriminatore A
+        # Discriminator A
+        elif optimizer_idx==1:
             # Real loss
             pred_real = self.netD_A(real_A)
             loss_D_real = self.criterion_GAN(pred_real, target_real)
 
             # Fake loss
-            fake_A = self.netG_B2A(real_B) # passa B a B2A - sono le fake A
+            fake_A = self.netG_B2A(real_B)
             pred_fake = self.netD_A(fake_A.detach())
             loss_D_fake = self.criterion_GAN(pred_fake, target_fake)
 
@@ -147,12 +144,13 @@ class CycleGAN(pl.LightningModule):
             self.log('loss_D/loss_D_A',loss_D_A, on_epoch=True)
             return loss_D_A
         
-        elif optimizer_idx==2: #discriminatore B
+        # Discriminator B
+        elif optimizer_idx==2:
             pred_real = self.netD_B(real_B)
             loss_D_real = self.criterion_GAN(pred_real, target_real)
 
             # Fake loss
-            fake_B = self.netG_A2B(real_A) # passa A ad A2B - sono le fake B
+            fake_B = self.netG_A2B(real_A) 
             pred_fake = self.netD_B(fake_B.detach())
             loss_D_fake = self.criterion_GAN(pred_fake, target_fake)
 
