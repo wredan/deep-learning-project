e715d8ef593afac1dca3148dec915abcb310bdaf
Danilo Santitto <danilo.santitto@gmail.com>
2022-11-15 20:41:18 +0100

    fix(Discriminator): add in_features parameter

--- scripts/ADDA/discriminator.py
+++ scripts/ADDA/discriminator.py
@@ -2,10 +2,10 @@ from torch import nn
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_features):
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(320, 50),
+            nn.Linear(in_features, 50),
             nn.ReLU(),
             nn.Linear(50, 20),
             nn.ReLU(),
